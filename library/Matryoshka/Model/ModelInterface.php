0315e74bf64b98eaa7b646815a04d446f2b9b7d4
leodido <leodidonato@gmail.com>
2014-02-17 23:39:12 +0100

    Added header comment blocks

--- library/Matryoshka/Model/ModelInterface.php
+++ library/Matryoshka/Model/ModelInterface.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Matryoshka
+ *
+ * @link        https://github.com/ripaclub/matryoshka
+ * @copyright   Copyright (c) 2014, Leonardo Di Donato <leodidonato at gmail dot com>, Leonardo Grasso <me at leonardograsso dot com>
+ * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
 namespace Matryoshka\Model;
 
 use Matryoshka\Model\Criteria\CriteriaInterface;
@@ -6,9 +13,11 @@ use Matryoshka\Model\ResultSet\ResultSetInterface;
 
 interface ModelInterface
 {
+
     /**
      * @param CriteriaInterface $criteria
      * @return ResultSetInterface
      */
     public function find(CriteriaInterface $criteria);
+
 }
