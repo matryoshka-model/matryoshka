027d1c614406612517028bff271915d829dc1e86
Leonardo Grasso <me@leonardograsso.com>
2014-04-02 18:22:16 +0200

    Added ModelAware trait and interface

---
+++ library/Matryoshka/Model/ModelAwareTrait.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Matryoshka
+ *
+ * @link        https://github.com/ripaclub/matryoshka
+ * @copyright   Copyright (c) 2014, Ripa Club
+ * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+namespace Matryoshka\Model;
+
+/**
+ * Trait ModelAwareTrait
+ */
+trait ModelAwareTrait
+{
+    /**
+     * Model
+     * @var ModelInterface
+     */
+    protected $model;
+
+    /**
+     * Set Model
+     * @param ModelInterface $model
+     * @return $this
+     */
+    public function setModel(ModelInterface $model)
+    {
+        $this->model = $model;
+        return $this;
+    }
+
+    /**
+     * Get Model
+     * @return ModelInterface
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+}
