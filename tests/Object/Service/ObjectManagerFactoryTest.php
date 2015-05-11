762e1e258ae56ee01e14de552cf00014f6c18696
Leonardo Grasso <me@leonardograsso.com>
2015-05-11 13:21:51 +0200

    Update copyright year

--- tests/Object/Service/ObjectManagerFactoryTest.php
+++ tests/Object/Service/ObjectManagerFactoryTest.php
@@ -3,7 +3,7 @@
  * Matryoshka
  *
  * @link        https://github.com/matryoshka-model/matryoshka
- * @copyright   Copyright (c) 2014, Ripa Club
+ * @copyright   Copyright (c) 2014-2015, Ripa Club
  * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
  */
 namespace MatryoshkaTest\Model\Object\Service;
