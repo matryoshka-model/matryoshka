19c139566f425b60a2d45c1034301f8d605a4ed3
Leonardo Grasso <me@leonardograsso.com>
2016-04-27 15:50:05 +0200

    Copyright (c) 2014-2016 [ci skip]

--- tests/Object/PrototypeStrategy/ServiceLocatorStrategyTest.php
+++ tests/Object/PrototypeStrategy/ServiceLocatorStrategyTest.php
@@ -3,7 +3,7 @@
  * Matryoshka
  *
  * @link        https://github.com/matryoshka-model/matryoshka
- * @copyright   Copyright (c) 2014-2015, Ripa Club
+ * @copyright   Copyright (c) 2014-2016, Ripa Club
  * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
  */
 namespace MatryoshkaTest\Model\ResultSet\PrototypeStrategy;
